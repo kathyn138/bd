192cbde7c7f3f636579a1e7f310caa0d5bbef723
Kathy Nguyen <kathyn262@gmail.com>
2020-11-25 23:41:19 -0800

    put back ge tooltip

--- dist/remote.js
+++ dist/remote.js
@@ -1798,7 +1798,26 @@
       }
   }
   const me = P.React;
-
+  class ge extends me.Component {
+    render() {
+        const e = this.props.size || "18px";
+        return me.createElement("svg", {
+            viewBox: "0 0 24 24",
+            fill: "#FFFFFF",
+            className: this.props.className || "",
+            style: {
+                width: e,
+                height: e
+            },
+            onClick: this.props.onClick
+        }, me.createElement("path", {
+            d: "M0 0h24v24H0z",
+            fill: "none"
+        }), me.createElement("path", {
+            d: "M13 3c-4.97 0-9 4.03-9 9H1l3.89 3.89.07.14L9 12H6c0-3.87 3.13-7 7-7s7 3.13 7 7-3.13 7-7 7c-1.93 0-3.68-.79-4.94-2.06l-1.42 1.42C8.27 19.99 10.51 21 13 21c4.97 0 9-4.03 9-9s-4.03-9-9-9zm-1 5v5l4.28 2.54.72-1.21-3.5-2.08V8H12z"
+        }))
+    }
+}
   const be = () => j("tooltip", "tooltipBlack"),
       fe = j("layer", "layerContainer"),
       ye = function (e) {
